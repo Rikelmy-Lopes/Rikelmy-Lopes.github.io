f6bf1ad52f0c4094906f51c0773da8532c32ecff
Rikelmy Lopes <rikelmylopes899@gmail.com>
2022-11-10 22:38:42 -0300

    adicionado animacoes e sobre

---
+++ js/scrollReveal.js
@@ -0,0 +1,18 @@
+ScrollReveal({
+    delay: 250,
+    reset: true,
+    duration: 1000,
+    useDelay: 'always',
+    origin: 'top',
+    viewFactor: 0.3
+})
+
+ScrollReveal().reveal('header');
+
+ScrollReveal().reveal('.inicial');
+
+ScrollReveal().reveal('#about-me-text');
+
+ScrollReveal().reveal('#me');
+
+ScrollReveal().reveal('#image-profile')
