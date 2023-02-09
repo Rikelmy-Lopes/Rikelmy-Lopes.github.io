5aeed89128ed2d6067468da80e11903e9d14033e
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-02-09 19:23:52 -0300

    removido o lazy loading

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Presentation from './components/Presentation';
-// import AboutMe from './components/About-Me';
-// import Projects from './components/Projects';
-// import ContactMe from './components/Contact-Me';
-// import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import ContactMe from './components/ContactMe';
+import Footer from './components/Footer';
 import { repos } from './config';
-
-const AboutMe = lazy(() => import('./components/AboutMe'));
-const Projects = lazy(() => import('./components/Projects'));
-const ContactMe = lazy(() => import('./components/ContactMe'));
-const Footer = lazy(() => import('./components/Footer'));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Header />
-        <Presentation />
-        <AboutMe />
-        <Projects 
-          heading={repos.heading}
-          username={repos.gitHubUsername}
-          length={repos.reposLength}
-          specfic={repos.specificRepos}/>
-        <ContactMe />
-        <Footer />
-      </Suspense>
+      <Header />
+      <Presentation />
+      <AboutMe />
+      <Projects 
+        heading={repos.heading}
+        username={repos.gitHubUsername}
+        length={repos.reposLength}
+        specfic={repos.specificRepos}/>
+      <ContactMe />
+      <Footer />
     </div>
   );
 }
